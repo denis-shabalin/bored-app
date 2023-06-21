8ebaebe3bc55a7c078aa4b757ad5e65a4dda696c
denis-shabalin <98424628+denis-shabalin@users.noreply.github.com>
2023-06-22 00:09:39 +0300

    add files

---
+++ index.js
@@ -0,0 +1,19 @@
+const buttonNode = document.querySelector('.bored__btn');
+const containerNode = document.querySelector('.container');
+const textNode = document.querySelector('.bored__text');
+const titleNode = document.querySelector('.bored__title');
+
+function getNewActivity() {
+   fetch('http://www.boredapi.com/api/activity/')
+      .then(response => response.json())
+      .then(data => {
+         textNode.innerHTML = data.activity;
+         containerNode.classList.add('container_green');
+         titleNode.innerText = 'Ура, теперь не скучно \u{1F525}';
+   })
+   .catch(error => {
+      console.error('Ошибка при выполнении запроса на API: ', error);
+   });
+};
+
+buttonNode.addEventListener('click', getNewActivity);

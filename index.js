80d2d59289d6ad6d20821d3f1cab4e5725704aa4
Denis Shabalin <shabalin.denis03@mail.ru>
2023-06-22 09:16:57 +0300

    update files and add favicon

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const buttonNode = document.querySelector('.bored__btn');
-const containerNode = document.querySelector('.container');
-const textNode = document.querySelector('.bored__text');
-const titleNode = document.querySelector('.bored__title');
+const buttonNode = document.querySelector('.js-btn');
+const containerNode = document.querySelector('.js-container');
+const textNode = document.querySelector('.js-text');
+const titleNode = document.querySelector('.js-title');
 
 function getNewActivity() {
    fetch('https://www.boredapi.com/api/activity/')
       .then(response => response.json())
       .then(data => {
          textNode.innerHTML = data.activity;
-         containerNode.classList.add('container_green');
-         titleNode.innerText = 'Ура, теперь не скучно \u{1F525}';
+         renderUpdatedAppearance();
    })
    .catch(error => {
       console.error('Ошибка при выполнении запроса на API: ', error);
    });
 };
+
+function renderUpdatedAppearance() {
+   containerNode.classList.add('container_green');
+   titleNode.innerText = 'Ура, теперь не скучно \u{1F525}';
+}
 
 buttonNode.addEventListener('click', getNewActivity);
